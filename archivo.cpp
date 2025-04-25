dc958a22a87f1063e035768f046fe7087e58f00a
gasaavedra909 <gasaavedra@espe.edu.ec>
2025-04-25 08:42:11 -0500

    Update archivo.cpp

--- archivo.cpp
+++ archivo.cpp
@@ -6,6 +6,15 @@ int main() {
 float suma, resta, multiplicacion, division;
 
 
+
+
+
+
+    suma=n1+n2;
+    resta=n1-n2;
+    multiplicacion= n1*n2;
+    division=n1/n2;
+    cout<<"la suma es:"<<suma<<endl;
     
     return 0;
 }
