6ebe86d651af3e74e798bc8126413efd5e14b92e
gasaavedra909 <gasaavedra@espe.edu.ec>
2025-04-25 08:50:22 -0500

    Update archivo.cpp

--- archivo.cpp
+++ archivo.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 float n1, n2, suma, resta, multiplicacion, division;
 cout<<"ingrese un numero: "; cin>>n1;
-cout<<"ingrese un numero:"; cin>>n2
+cout<<"ingrese un numero:"; cin>>n2;
     suma=n1+n2;
     resta=n1-n2;
     multiplicacion= n1*n2;
