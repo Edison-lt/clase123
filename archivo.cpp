90836c83dedf3be868529d8e8cebb0aa22e18366
Edison-lt <eschicaiza3@espe.edu.ec>
2025-04-25 08:47:23 -0500

    Update archivo.cpp

--- archivo.cpp
+++ archivo.cpp
@@ -3,14 +3,9 @@
 using namespace std;
 
 int main() {
-float suma, resta, multiplicacion, division;
+float n1, n2, suma, resta, multiplicacion, division;
 cout<<"ingrese un numero: "; cin>>n1;
 cout<<"ingrese un numero:"; cin>>n2
-
-
-
-
-
     suma=n1+n2;
     resta=n1-n2;
     multiplicacion= n1*n2;
